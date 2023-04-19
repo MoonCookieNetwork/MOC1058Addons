ff6b0167049340fe3ecdbf82f63ad50ff8143ebf
YukiEnd233 <85435560+YukiEnd233@users.noreply.github.com>
2023-04-19 20:55:29 +0800

    更换Message发送方式

--- src/main/java/cn/mooncookie/bw1058addons/TaskMessage/GameStatesListener.java
+++ src/main/java/cn/mooncookie/bw1058addons/TaskMessage/GameStatesListener.java
@@ -3,7 +3,6 @@ package cn.mooncookie.bw1058addons.TaskMessage;
 import cn.mooncookie.bw1058addons.BedWars1058Addons;
 import com.andrei1058.bedwars.api.arena.GameState;
 import com.andrei1058.bedwars.api.events.gameplay.GameStateChangeEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitTask;
@@ -14,13 +13,6 @@ public class GameStatesListener implements Listener {
     public void onGameStart(GameStateChangeEvent e) {
         if (e.getNewState().equals(GameState.playing)) {
             BukkitTask RejoinMessage = new Message(BedWars1058Addons.getInstance()).runTaskTimer(BedWars1058Addons.getInstance(), 2400L, 3600L);
-        }
-    }
-
-    @EventHandler
-    public void onGameEnd(GameStateChangeEvent e) {
-        if (e.getNewState().equals(GameState.restarting)) {
-            Bukkit.getScheduler().cancelAllTasks();
         }
     }
 }
--- src/main/java/cn/mooncookie/bw1058addons/TaskMessage/Message.java
+++ src/main/java/cn/mooncookie/bw1058addons/TaskMessage/Message.java
@@ -1,6 +1,7 @@
 package cn.mooncookie.bw1058addons.TaskMessage;
 
 import cn.mooncookie.bw1058addons.BedWars1058Addons;
+import com.andrei1058.bedwars.arena.Arena;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -16,7 +17,9 @@ public class Message extends BukkitRunnable {
     @Override
     public void run() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            player.sendMessage("§c§l如果你断开连接， 可在起床大厅输入/rejoin重新加入游戏。");
+            if (Arena.getArenaByPlayer(player).isPlayer(player)) {
+                player.sendMessage("§c§l如果你断开连接， 可在起床大厅输入/rejoin重新加入游戏。");
+            }
         }
     }
 }
